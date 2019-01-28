0cdb7fd7ea898a08d7aab00a8b048bfa1054298c
NikhilRajPandey <nikhilrajpandey1@gmail.com>
2019-01-28 10:03:18 +0530

    Commenting something and testing output

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-Mylist = [1,2,5,6,88,88,2,2]
+Mylist = [1,2,5,6,88,88,2]
 Mydict = {"allo":"Bakra","mura":"bhindi"}
 
 def _mode(paramlist):
@@ -24,20 +24,22 @@ def _mode(paramlist):
         else:
             Value_of_current_num = 0
             Result.update({f"{i}":f"{Value_of_current_num}"})
+        
+        print(Result)
 
     # Giving The Result
-    Result_procces = []
+    # Result_procces = []
 
-    for num in Result.keys():
-        Result_procces.append(num)
+    # for num in Result.keys():
+    #     Result_procces.append(num)
 
-    max_val_of_result_procces = max(Result_procces)
+    # max_val_of_result_procces = max(Result_procces)
 
-    for keys in Result:
-        resultskeys = keys.find(max_val_of_result_procces)
+    # for keys in Result:
+    #     resultskeys = keys.find(max_val_of_result_procces)
 
-        if resultskeys != -1:
-            print(keys)
+        # if resultskeys != -1:
+        #     print(keys)
 
 _mode(Mylist)
 
