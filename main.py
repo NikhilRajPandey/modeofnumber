e90aa317c4c0de82b968dc3968d8923dbdda5a35
NikhilRajPandey <nikhilrajpandey1@gmail.com>
2019-01-28 19:53:56 +0530

    Solve the bug of Try to count

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # all testing list and dictoniary
-Mylist = [1,2,5,6,88,88,2,2]
+Mylist = [1,2,5,6,88,88,2,2,98]
 Mydict = {"allo":"Bakra","mura":"bhindi"}
 
 def _mode(paramlist):
@@ -8,8 +8,10 @@ def _mode(paramlist):
     Mylist_min = min(Mylist)
 
     Result = {}
+    Result_list_proccess = []
     Min_value = 0
     Max_value = 0
+    Value_of_current_num = 0
 
     # Try to count
     for i in paramlist:
@@ -22,24 +24,34 @@ def _mode(paramlist):
             Result.update({f"{Mylist_max}":f"{Max_value}"})
         
         else:
-            Value_of_current_num = 1
-            Result.update({f"{i}":f"{Value_of_current_num}"})
-        
+            # print(Result.keys())
+            if i in Result_list_proccess:
+                string_i = str(i)
+                Result[string_i] = int(Result[string_i]) + 1
+                # print(Result[i])
+                # print("True")
+            else:
+                Value_of_current_num = 0
+                Value_of_current_num = Value_of_current_num + 1
+                Result.update({f"{i}":f"{Value_of_current_num}"})
+                Result_list_proccess.append(i)
+            # print(Value_of_current_num)
+
     print(Result)
 
     # Giving The Result
     # Result_procces = []
 
-    # for num in Result.keys():
-    #     Result_procces.append(num)
+    # for num in Result.values():
+    #     print(num)
 
     # max_val_of_result_procces = max(Result_procces)
 
     # for keys in Result:
     #     resultskeys = keys.find(max_val_of_result_procces)
 
-        # if resultskeys != -1:
-        #     print(keys)
+    #     if resultskeys != -1:
+    #         print(keys)
 
 _mode(Mylist)
 
